6a39d7d445b2ea5fc953abaa0285098f4680471c
Arfon Smith <arfon.smith@gmail.com>
2012-11-17 16:20:49 -0600

    Form nastiness

--- app/uploaders/uploader.rb
+++ app/uploaders/uploader.rb
@@ -1,11 +1,20 @@
 class Uploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   
-  version :standard do
-    process :resize_to_fill => [600, 400, :north]
+  version :carousel do
+    process :resize_to_fit => [1040, 350]
+  end
+  
+  version :large do
+    process, :resize_to_fit => [490, 350]
+  end
+  
+  version :school do
+    process, :resize_to_fit => [235, 175]
   end
   
   version :thumbnail do
     process :resize_to_fit => [50, 50]
   end
 end
+

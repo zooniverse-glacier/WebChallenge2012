9506ab0792f007fab91cd6df5ca95cc041252388
Arfon Smith <arfon.smith@gmail.com>
2012-11-17 09:22:52 -0600

    Initial Rails application

---
+++ pangea/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pangea::Application.config.secret_token = 'ceeeb35878dc2659ca74222f97b791b671bb75acc9c7634896175fb480226fb2ba2e0a285c1d4c66767394002e48317fb0dd7d79b49bcee7c8996c072350fea8'

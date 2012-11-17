245c3648924c372bec2a1167055a7ca62f56e507
Michael Parrish <michael@zooniverse.org>
2012-11-17 09:42:10 -0600

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pangea::Application.config.secret_token = '395bb389a72fdcb31f9be06c237a4b3e4956ff852447d3798d66823f85ec0dc1789b408b6d367fcf1d98ad23ce07c3ada77c297a60d82653594a5174ed040c78'

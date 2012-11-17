4424e20ded3ac90511417d24aecc3b5f5c716a27
Michael Parrish <michael@zooniverse.org>
2012-11-17 10:01:17 -0600

    Bundle and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117155248) do
+ActiveRecord::Schema.define(:version => 20121117160010) do
 
   create_table "universities", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(:version => 20121117155248) do
     t.string   "contact"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
